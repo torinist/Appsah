0610c37f08e9c962570efff137629a98ca6b0f81
oki mih <torino.torinist@gmail.com>
2015-11-23 21:49:54 +0900

    フリーソフトの管理機能追加を開始

---
+++ src/main/java/backingbean/FreesoftManagementBackingBean.java
@@ -0,0 +1,14 @@
+package backingbean;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named(value="FSManageBB")
+@ViewScoped
+public class FreesoftManagementBackingBean {
+
+	
+	public void search() {
+		// キーワードかカテゴリで検索
+	}
+}
